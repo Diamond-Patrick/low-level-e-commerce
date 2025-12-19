55ad71a592d5a590f94ac5c1e0233f2c74f89a9f
Diamond Patrick <diamondpaatrick@gmail.com>
2025-12-19 20:42:35 +0700

    view : create html file (mainPage.html), create servlet class (MainPage.java) for intergrated with html file and doing test

---
+++ src/main/java/com/e_commerce_low_level/low_level_e_commerce/View/Customer/MainPage.java
@@ -0,0 +1,58 @@
+package com.e_commerce_low_level.low_level_e_commerce.View.Customer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+
+import com.e_commerce_low_level.low_level_e_commerce.Repository.Product.ProductRepo;
+import com.e_commerce_low_level.low_level_e_commerce.Repository.Product.ProductRepoImpl;
+import com.e_commerce_low_level.low_level_e_commerce.Service.Product.ProductService;
+import com.e_commerce_low_level.low_level_e_commerce.Service.Product.ProductServiceImpl;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/products")
+public class MainPage extends HttpServlet {
+
+    private ProductRepo productRepo = new ProductRepoImpl();
+    private ProductService productService = new ProductServiceImpl(productRepo);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Path of = Path.of("src/main/resources/html/Customer/mainPage.html");
+
+        String html = Files.readString(of);
+
+        String card = """
+                <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="data:image/png;base64,%s" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">%s</h5>
+                            <p class="card-text">%s</p>
+                            <a class="btn btn-primary">%s</a>
+                        </div>
+                    </div>
+                </div>
+                    """;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        productService.findAll().forEach(t -> {
+            stringBuilder.append(String.format(card,
+                    Base64.getEncoder().encodeToString(t.getGambar()),
+                    t.getName(),
+                    t.getDescription(),
+                    t.getHarga()));
+        });
+
+        String result = html.replace("$test", stringBuilder.toString());
+
+        resp.getWriter().println(result);
+    }
+}
