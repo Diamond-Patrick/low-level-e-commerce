508c9207e58f4052d25d01175765dc33fb729de9
Diamond Patrick <diamondpaatrick@gmail.com>
2025-12-21 14:43:27 +0700

    view: create login.html file for login page and create midleware class (Login.java) to recognise the  user already login or not

---
+++ src/main/java/com/e_commerce_low_level/low_level_e_commerce/View/Login.java
@@ -0,0 +1,39 @@
+package com.e_commerce_low_level.low_level_e_commerce.View;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns = "/*")
+public class Login extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        if (request.getRequestURI().equals("/login")) {
+            chain.doFilter(request, response); // continue
+
+        } else {
+
+            Cookie[] cookies = request.getCookies();
+
+            HttpSession session = request.getSession();
+            Object attribute = session.getAttribute("id");
+
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().isBlank() && attribute == null) {
+                    response.sendRedirect("/login");
+                }
+            }
+
+        }
+
+    }
+}
