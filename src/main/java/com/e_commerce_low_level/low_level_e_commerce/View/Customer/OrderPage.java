9d2470bb47cf0dacdd89cf8380d23d4ad9cad25b
Diamond Patrick <diamondpaatrick@gmail.com>
2025-12-29 15:29:55 +0700

    view : implement logic for order page, show all order products depends by customer data. and doing test

---
+++ src/main/java/com/e_commerce_low_level/low_level_e_commerce/View/Customer/OrderPage.java
@@ -0,0 +1,70 @@
+package com.e_commerce_low_level.low_level_e_commerce.View.Customer;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.e_commerce_low_level.low_level_e_commerce.Repository.Order.OrderRepo;
+import com.e_commerce_low_level.low_level_e_commerce.Repository.Order.OrderRepoImpl;
+import com.e_commerce_low_level.low_level_e_commerce.Service.Order.OrderService;
+import com.e_commerce_low_level.low_level_e_commerce.Service.Order.OrderServiceImpl;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/order")
+public class OrderPage extends HttpServlet {
+
+    private OrderRepo orderRepo = new OrderRepoImpl();
+    private OrderService orderService = new OrderServiceImpl(orderRepo);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Path of = Path.of("src/main/resources/html/Customer/orderPage.html");
+
+        String string = Files.readString(of);
+
+        String cardProduct = """
+                <div class="p-6 max-w-sm mx-auto my-8 bg-white rounded-xl shadow-lg shadow-slate-500 flex items-center space-x-4">
+                    <div>
+                        <div class="text-xl font-medium text-black">%s</div>
+                        <p class="text-slate-500"><a class="font-semibold text-black">Price:</a> %s</p>
+                        <p class="text-slate-500"><a class="font-semibold text-black">Purchase On:</a> %s</p>
+                        <div>
+                            <button class="w-auto h-10 block mx-auto mt-1 border rounded-full
+                                    bg-sky-500 text-white font-semibold hover:bg-sky-600
+                                    focus:bg-pink-700 focus:ring-4 focus:ring-sky-200
+                                    hover:cursor-pointer" type="submit"><a class="mx-3">Product have been received</a></button>
+                        </div>
+                    </div>
+                </div>
+                """;
+
+        Cookie[] cookies = req.getCookies();
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        orderService.findAll().forEach(t -> {
+            for (Cookie cookie : cookies) {
+                if (t.getIdCustomer().getIdCustomer().equals(cookie.getValue())) {
+
+                    stringBuilder.append(String.format(
+                            cardProduct,
+                            t.getKodeProduct().getName(),
+                            t.getKodeProduct().getHarga().toString(),
+                            t.getPurchaceDate().toString()));
+
+                }
+            }
+        });
+
+        String replace = string.replace("$cardProduct", stringBuilder);
+
+        resp.getWriter().println(replace);
+    }
+
+}
