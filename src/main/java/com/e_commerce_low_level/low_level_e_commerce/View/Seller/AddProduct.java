ac03d94151727ae5256ba1fe29dc27303f0dcfbb
Diamond Patrick <diamondpaatrick@gmail.com>
2026-01-08 22:15:38 +0700

    view : implemant logic for doPost method in AddProduct class

--- src/main/java/com/e_commerce_low_level/low_level_e_commerce/View/Seller/AddProduct.java
+++ src/main/java/com/e_commerce_low_level/low_level_e_commerce/View/Seller/AddProduct.java
@@ -4,14 +4,27 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import com.e_commerce_low_level.low_level_e_commerce.Entity.ProductEntity;
+import com.e_commerce_low_level.low_level_e_commerce.Repository.Product.ProductRepo;
+import com.e_commerce_low_level.low_level_e_commerce.Repository.Product.ProductRepoImpl;
+import com.e_commerce_low_level.low_level_e_commerce.Service.Product.ProductService;
+import com.e_commerce_low_level.low_level_e_commerce.Service.Product.ProductServiceImpl;
+
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
 @WebServlet(urlPatterns = "/addproduct")
+@MultipartConfig
 public class AddProduct extends HttpServlet {
+
+    private ProductRepo productRepo = new ProductRepoImpl();
+    private ProductService productService = new ProductServiceImpl(productRepo);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -22,4 +35,42 @@ public class AddProduct extends HttpServlet {
         resp.getWriter().println(string);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String kodeBarang = req.getParameter("kodeBarang");
+        String namaBarang = req.getParameter("namaBarang");
+        String harga = req.getParameter("harga");
+        String stock = req.getParameter("stock");
+        String description = req.getParameter("description");
+        Part gambar = req.getPart("gambar");
+
+        byte[] gambarByte = gambar.getInputStream().readAllBytes();
+
+        ProductEntity productEntity = new ProductEntity();
+        productEntity.setKodeProduct(kodeBarang);
+        productEntity.setName(namaBarang);
+        productEntity.setHarga(Double.parseDouble(harga));
+        productEntity.setStock(Integer.parseInt(stock));
+        productEntity.setDescription(description);
+        productEntity.setGambar(gambarByte);
+
+        boolean insert = productService.insert(productEntity);
+
+        if (insert) {
+
+            Cookie[] cookies = req.getCookies();
+
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("id")) {
+
+                    productService.insertOmset(kodeBarang, cookie.getValue());
+                }
+            }
+            resp.sendRedirect("/productselling");
+
+        } else {
+            resp.sendRedirect(req.getRequestURI());
+        }
+
+    }
 }
